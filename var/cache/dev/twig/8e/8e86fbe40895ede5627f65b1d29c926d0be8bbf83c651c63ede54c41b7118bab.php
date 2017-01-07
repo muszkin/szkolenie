71a02ee3f9cd6bb025b33ad6ef8dc93c07d891df
muszkin <piotr.mucha@shoper.pl>
2017-01-07 23:01:46 +0100

    add route to /

--- var/cache/dev/twig/8e/8e86fbe40895ede5627f65b1d29c926d0be8bbf83c651c63ede54c41b7118bab.php
+++ var/cache/dev/twig/8e/8e86fbe40895ede5627f65b1d29c926d0be8bbf83c651c63ede54c41b7118bab.php
@@ -21,44 +21,44 @@ class __TwigTemplate_1556ea06b3e6873bef146f398e817c92fd1eafdb565cf02de88e85ed7db
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_ba20e535cb3c0995f9d17c69d6e8af159a82505a4e5cbd1304fc376d5003636c = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
-        $__internal_ba20e535cb3c0995f9d17c69d6e8af159a82505a4e5cbd1304fc376d5003636c->enter($__internal_ba20e535cb3c0995f9d17c69d6e8af159a82505a4e5cbd1304fc376d5003636c_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:Index:index.html.twig"));
+        $__internal_06653460813ede7ef7d3fde0f26ad61777531981a5e4b2f80060dbecff22a74a = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_06653460813ede7ef7d3fde0f26ad61777531981a5e4b2f80060dbecff22a74a->enter($__internal_06653460813ede7ef7d3fde0f26ad61777531981a5e4b2f80060dbecff22a74a_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:Index:index.html.twig"));
 
-        $__internal_fd385701c8776a9c3dfabded65f93e23ac23d34d3971b990bca142b3652523ad = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_fd385701c8776a9c3dfabded65f93e23ac23d34d3971b990bca142b3652523ad->enter($__internal_fd385701c8776a9c3dfabded65f93e23ac23d34d3971b990bca142b3652523ad_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:Index:index.html.twig"));
+        $__internal_3c04e5a1b0d2331e5a1eea9ad721addac0e79844c6aa53342a0a33e97108163e = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_3c04e5a1b0d2331e5a1eea9ad721addac0e79844c6aa53342a0a33e97108163e->enter($__internal_3c04e5a1b0d2331e5a1eea9ad721addac0e79844c6aa53342a0a33e97108163e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:Index:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_ba20e535cb3c0995f9d17c69d6e8af159a82505a4e5cbd1304fc376d5003636c->leave($__internal_ba20e535cb3c0995f9d17c69d6e8af159a82505a4e5cbd1304fc376d5003636c_prof);
+        $__internal_06653460813ede7ef7d3fde0f26ad61777531981a5e4b2f80060dbecff22a74a->leave($__internal_06653460813ede7ef7d3fde0f26ad61777531981a5e4b2f80060dbecff22a74a_prof);
 
         
-        $__internal_fd385701c8776a9c3dfabded65f93e23ac23d34d3971b990bca142b3652523ad->leave($__internal_fd385701c8776a9c3dfabded65f93e23ac23d34d3971b990bca142b3652523ad_prof);
+        $__internal_3c04e5a1b0d2331e5a1eea9ad721addac0e79844c6aa53342a0a33e97108163e->leave($__internal_3c04e5a1b0d2331e5a1eea9ad721addac0e79844c6aa53342a0a33e97108163e_prof);
 
     }
 
     // line 3
     public function block_body($context, array $blocks = array())
     {
-        $__internal_754e20ed5cd923d3d4a30cfa7078e9f725c03a14d1901a05eb4150aeac5869fc = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
-        $__internal_754e20ed5cd923d3d4a30cfa7078e9f725c03a14d1901a05eb4150aeac5869fc->enter($__internal_754e20ed5cd923d3d4a30cfa7078e9f725c03a14d1901a05eb4150aeac5869fc_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_9f9c8d8d1761e17516679ee689c4db142b9e0f434aa4ec6bc30cdd54135371ed = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_9f9c8d8d1761e17516679ee689c4db142b9e0f434aa4ec6bc30cdd54135371ed->enter($__internal_9f9c8d8d1761e17516679ee689c4db142b9e0f434aa4ec6bc30cdd54135371ed_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
-        $__internal_0dbbdca5cfff3bd9c22dda19209bf794cb722e1a8770cc4a5408e783ee9736f9 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_0dbbdca5cfff3bd9c22dda19209bf794cb722e1a8770cc4a5408e783ee9736f9->enter($__internal_0dbbdca5cfff3bd9c22dda19209bf794cb722e1a8770cc4a5408e783ee9736f9_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_0ef0f1ef0b390573fb47c7fa6521bb4e942617dd30afca3a8ac169187a0643d3 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_0ef0f1ef0b390573fb47c7fa6521bb4e942617dd30afca3a8ac169187a0643d3->enter($__internal_0ef0f1ef0b390573fb47c7fa6521bb4e942617dd30afca3a8ac169187a0643d3_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
         echo "
     <p><a href=\"";
         // line 5
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("listAction");
-        echo "\">Books</a> - <a href=\"";
+        echo "\">Books </a> - <a href=\"";
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("category_index");
         echo "\">Categories</a></p>
 ";
         
-        $__internal_0dbbdca5cfff3bd9c22dda19209bf794cb722e1a8770cc4a5408e783ee9736f9->leave($__internal_0dbbdca5cfff3bd9c22dda19209bf794cb722e1a8770cc4a5408e783ee9736f9_prof);
+        $__internal_0ef0f1ef0b390573fb47c7fa6521bb4e942617dd30afca3a8ac169187a0643d3->leave($__internal_0ef0f1ef0b390573fb47c7fa6521bb4e942617dd30afca3a8ac169187a0643d3_prof);
 
         
-        $__internal_754e20ed5cd923d3d4a30cfa7078e9f725c03a14d1901a05eb4150aeac5869fc->leave($__internal_754e20ed5cd923d3d4a30cfa7078e9f725c03a14d1901a05eb4150aeac5869fc_prof);
+        $__internal_9f9c8d8d1761e17516679ee689c4db142b9e0f434aa4ec6bc30cdd54135371ed->leave($__internal_9f9c8d8d1761e17516679ee689c4db142b9e0f434aa4ec6bc30cdd54135371ed_prof);
 
     }
 
@@ -91,7 +91,7 @@ class __TwigTemplate_1556ea06b3e6873bef146f398e817c92fd1eafdb565cf02de88e85ed7db
 
 {% block body %}
 
-    <p><a href=\"{{ path(\"listAction\") }}\">Books</a> - <a href=\"{{ path(\"category_index\") }}\">Categories</a></p>
+    <p><a href=\"{{ path(\"listAction\") }}\">Books </a> - <a href=\"{{ path(\"category_index\") }}\">Categories</a></p>
 {% endblock %}", "AppBundle:Index:index.html.twig", "/home/muszkin/public_html/szkolenie/src/AppBundle/Resources/views/Index/index.html.twig");
     }
 }
